5e0ec1d1639e761e4b38ca8eba54fb79e5512d7f
Documenter.jl <documenter@juliadocs.github.io>
2021-08-03 14:58:38 +0000

    build based on d0dab59

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MovingWindow","category":"page"},{"location":"#MovingWindow","page":"Home","title":"MovingWindow","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MovingWindow.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MovingWindow]","category":"page"}]
+}
