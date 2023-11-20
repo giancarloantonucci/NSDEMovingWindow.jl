1204e31dfbf98edd13d9632ad0077967d800fbe0
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2023-11-20 10:10:06 +0000

    Remove AbstractBudget

--- src/abstract.jl
+++ src/abstract.jl
@@ -2,5 +2,5 @@ abstract type AbstractMovingWindowSolver <: AbstractInitialValueSolver end
 abstract type AbstractMovingWindowSolution <: AbstractInitialValueSolution end
 abstract type AbstractMovingWindowCache <: AbstractInitialValueCache end
 abstract type AbstractMovingWindowParameters <: AbstractInitialValueParameters end
-abstract type AbstractBudget <: AbstractMovingWindowParameters end
-abstract type AbstractAdaptiveParameters <: AbstractMovingWindowParameters end
+
+abstract type AbstractAdaptiveMoWiParameters <: AbstractMovingWindowParameters end
