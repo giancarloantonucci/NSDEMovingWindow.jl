76e49a6d0647953d6f249f957e6ab44f44fe4807
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2023-11-21 21:34:53 +0000

    coarsolver > coarsesolver, ecc.

--- src/mowi/solve.jl
+++ src/mowi/solve.jl
@@ -1,15 +1,15 @@
-function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProblem)
+function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProblem; mode::String="SERIAL")
     @↓ parallelsolver, adaptive, τ, Δτ = mowi
     if adaptive !== nothing
         @↓ δτ⁻, δτ⁺, δΔτ⁻, δΔτ⁺, R, fineupdate = adaptive
     end
 
-    @↓ finesolver, coarsolver, tolerance = parallelsolver
+    @↓ finesolver, coarsesolver, tolerance = parallelsolver
     @↓ N = parallelsolver.parameters
 
     @↓ hF ← h = finesolver.stepsize
     hF₀ = hF
-    @↓ hG ← h = coarsolver.stepsize
+    @↓ hG ← h = coarsesolver.stepsize
     hG₀ = hG
 
     @↓ ϵ = tolerance
@@ -25,7 +25,7 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
     m = 1
     M = M₀ = length(windows)
 
-    cache = TimeParallel.TimeParallelCache(problem, parallelsolver)
+    cache = NSDETimeParallel.TimeParallelCache(problem, parallelsolver)
     @↓ skips, U, T = cache
 
     flag = true
@@ -47,8 +47,8 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
             U[1] = u0
             skips[1] = true
             for n = 1:N-1
-                windowchunkproblem = subproblemof(problem, U[n], T[n], T[n+1])
-                windowchunksolution = coarsolver(windowchunkproblem)
+                windowchunkproblem = copy(problem, U[n], T[n], T[n+1])
+                windowchunksolution = coarsesolver(windowchunkproblem)
                 U[n+1] = windowchunksolution(T[n+1])
                 skips[n+1] = true
             end
@@ -68,8 +68,8 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
                         skips[n] = false
                     end
                 else
-                    windowchunkproblem = subproblemof(problem, U[n-1], T[n-1], T[n])
-                    U[n] = coarsolver(windowchunkproblem).u[end]
+                    windowchunkproblem = copy(problem, U[n-1], T[n-1], T[n])
+                    U[n] = coarsesolver(windowchunkproblem).u[end]
                     skips[n] = true
                 end
             end
@@ -77,8 +77,8 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
         υ0 = U[1]
 
         # time-parallel subroutine (parallel)
-        windowproblem = subproblemof(problem, υ0, τ0, τN)
-        windows[m] = parallelsolver(cache, windowproblem)
+        windowproblem = copy(problem, υ0, τ0, τN)
+        windows[m] = parallelsolver(cache, windowproblem; mode)
 
         τJ = τN # update τJ for next iteration (next window or restart)
 
@@ -100,7 +100,7 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
                     @↑ finesolver.stepsize = h ← hF
                 end
                 hG = max(hG * δτ⁻, hF)
-                @↑ coarsolver.stepsize = h ← hG
+                @↑ coarsesolver.stepsize = h ← hG
 
                 # Δτ = max(Δτ * δΔτ⁻, τ / N)
 
@@ -116,7 +116,7 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
                     @↑ finesolver.stepsize = h ← hF
                 end
                 hG *= δτ⁺
-                @↑ coarsolver.stepsize = h ← hG
+                @↑ coarsesolver.stepsize = h ← hG
 
                 # Δτ = min(Δτ * δΔτ⁺, τ) # avoids overshoot
 
@@ -144,7 +144,7 @@ function (mowi::MoWi)(solution::MoWiSolution, problem::AbstractInitialValueProbl
     # @↑ solution = windows, restarts
 
     @↑ finesolver.stepsize = h ← hF₀
-    @↑ coarsolver.stepsize = h ← hG₀
+    @↑ coarsesolver.stepsize = h ← hG₀
 
     return solution
 end
