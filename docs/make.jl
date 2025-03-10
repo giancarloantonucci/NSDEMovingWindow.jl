bde2c50e96bd89e7f1a7f49414cd1dac28e18063
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2025-03-10 21:05:12 +0000

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,27 @@
 using Documenter
-using NSDEMovingWindow
+using DocumenterInterLinks
+using NSDEBase, NSDETimeParallel, NSDEMovingWindow
 
 PAGES = ["Home" => "index.md"]
+
+links = InterLinks(
+    "NSDEBase" => (
+        "https://giancarloantonucci.github.io/NSDEBase.jl/dev/",
+        "https://giancarloantonucci.github.io/NSDEBase.jl/dev/objects.inv"
+    ),
+    "NSDETimeParallel" => (
+        "https://giancarloantonucci.github.io/NSDETimeParallel.jl/dev/",
+        "https://giancarloantonucci.github.io/NSDETimeParallel.jl/dev/objects.inv"
+    )
+)
 
 makedocs(;
     sitename = "NSDEMovingWindow.jl",
     format = Documenter.HTML(),
     modules = [NSDEMovingWindow],
     pages = PAGES,
-    authors = "Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>"
+    authors = "Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>",
+    plugins = [links],
 )
 
 deploydocs(;
