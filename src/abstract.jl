e9196f25e91c532b9330913bf0b396d23e2478b9
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2025-03-10 20:34:56 +0000

    Add docstrings

--- src/abstract.jl
+++ src/abstract.jl
@@ -1,5 +1,14 @@
+"An abstract type for the [`MoWi`](@ref) algorithm."
 abstract type AbstractMovingWindowSolver <: AbstractInitialValueSolver end
+
+"An abstract type for solutions computed with [`MoWi`](@ref)."
 abstract type AbstractMovingWindowSolution <: AbstractInitialValueSolution end
+
+"An abstract type for caching intermediate computations in [`MoWi`](@ref)."
 abstract type AbstractMovingWindowCache <: AbstractInitialValueCache end
+
+"An abstract type for parameters in [`MoWi`](@ref)."
 abstract type AbstractMovingWindowParameters <: AbstractInitialValueParameters end
+
+"An abstract type for adaptive parameters in [`MoWi`](@ref)."
 abstract type AbstractAdaptiveMoWiParameters <: AbstractMovingWindowParameters end
